60d824932c52a095018bbb9503e13e4e08794681
Omar Yepez <ypz.omar@gmail.com>
2025-10-13 00:26:40 -0600

    fix contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -53,22 +53,6 @@ export default function Home() {
                         <div className="contact-three__inner w-80">
                             <div className="contact-three__right w-80">
                                 <h3 className="contact-three__form-title text-center mb-4">¡Cotiza Ahora!</h3>
-
-        {/*Contact Three Start*/}
-        <section className="contact-three">
-            <div className="container">
-                <div className="contact-three__inner">
-                    <div className="row">
-                        {/* <div className="col-xl-6">
-                            <div className="contact-three__left">
-                                <iframe
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4562.753041141002!2d-118.80123790098536!3d34.152323469614075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80e82469c2162619%3A0xba03efb7998eef6d!2sCostco+Wholesale!5e0!3m2!1sbn!2sbd!4v1562518641290!5m2!1sbn!2sbd"
-                                    className="google-map__one" ></iframe>
-                            </div>
-                        </div> */}
-                        <div className="col-xl-6">
-                            <div className="contact-three__right">
-                                <h3 className="contact-three__form-title">Cotiza Ahora</h3>
                                 <form id="contact-form" className="contact-form-validated contact-three__form"
                                     action="assets/mail.php" method="POST">
                                     <div className="row">
