bfdb9952d3218d164b01adfad0e11bbb3eeff621
Omar Yepez <ypz.omar@gmail.com>
2025-11-07 18:52:36 -0600

    update email in contact

--- app/contact/page.js
+++ app/contact/page.js
@@ -30,7 +30,7 @@ export default function Home() {
                                 <span className="icon-envelope"></span>
                             </div>
                             <p>Escribenos</p>
-                            <h3><Link href="mailto:example@gamil.com">rohder@gmail.com</Link></h3>
+                            <h3><Link href="mailto:example@gamil.com">ventas@rodheringenieria.com</Link></h3>
                         </div>
                     </div>
                     {/*Contact Two Single End*/}
